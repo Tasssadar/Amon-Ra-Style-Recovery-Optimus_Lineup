35bac567a396c76617e6b3c844f6a5bf38af73fd
Your Name <getitnowmarketing@gmail.com>
2011-11-05 10:21:17 -0400

    update to 3.06 many changes
    Conflicts:
    
    	recovery.c
    
    Conflicts:
    
    	extracommands.c
    	extracommands.h
    	recovery.c
    	ui.c

--- extracommands.h
+++ extracommands.h
@@ -1,4 +1,4 @@
-static int signature_check_enabled = 1;
+extern int signature_check_enabled;
 
 void
 toggle_signature_check();
@@ -15,18 +15,12 @@ __system(const char *command);
 int
 format_non_mtd_device(const char* root);
 
-//void usb_toggle_emmc();
-
-void 
-usb_toggle_sdcard();
 
 void
 wipe_battery_stats();
 
 void
 wipe_rotate_settings();
-
-//void show_choose_zip_menu_emmc();
 
 void 
 key_logger_test();
@@ -39,4 +33,53 @@ char *get_battery_info();
 char *get_info_bar();
 
 struct tm * getLocTime();
+
+void
+check_my_battery_level();
+
+void
+make_clockwork_path();
+
+void
+unpack_boot();
+
+int
+dump_device(const char *device);
+
+void
+setup_mkboot();
+
+int
+check_file_exists(const char* file_path);
+
+int
+copy_file(const char* source, const char* dest);
+
+void
+do_module();
+
+void
+do_make_new_boot();
+
+int
+is_dir(const char* file_path);
+
+int
+delete_file(const char* file);
+
+void
+install_su(int eng_su);
+
+void
+rb_bootloader();
+
+void
+rb_recovery();
+
+#ifdef USE_BIGDATA
+
+void
+bigdata_cleanup();
+
+#endif
 
